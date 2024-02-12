e31750ad5e975b8e406532ab47cd97578ea0726a
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-12 23:53:54 +0600

    unit testing

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,47 @@
+# api\tests.py
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from status.models import Status
 
 # Create your tests here.
+# function which will be tested
+# def add(a, b):
+#     return a + b
+
+
+# class TestAddTwoValue(TestCase):
+#     # test add fn
+#     # test fn_name name hbe test_fn_name dte hbe
+#     # must "test_" likhte hbe fn name er age
+#     def test_add(self):
+#         sum = add(3, 5)
+#         print(sum)
+#         # to set expected test case
+#         self.assertEqual(sum, 8)
+
+
+# from myproject.apps.core.models import Account
+
+
+class StatusTests(APITestCase):
+
+    # setUp fn age call hbe auto
+    def setUp(self):
+        # create a new user, cz status create korte user lagbe
+        new_user = User(username="rafid1", password="11111")
+        new_user.save()
+
+        # create new status
+        status = Status(text="sample text-3", user=new_user)
+        status.save()
+
+    def test_create_account(self):
+        url = reverse("status_view")
+        # get request
+        response = self.client.get(url, format="json")
+        print(response)
+        self.assertEqual(response, None)
--- api/urls.py
+++ api/urls.py
@@ -10,8 +10,8 @@ from status.views import (
 )
 
 urlpatterns = [
-    path("status/<int:id>/", StatusViewer.as_view(), name="status_view"),
-    path("statuses/", StatusListView.as_view()),
+    # path("status/<int:id>/", StatusViewer.as_view(), name="status_view"),
+    path("statuses/", StatusListView.as_view(), name="status_view"),
     path("status/create/", StatusCreateView.as_view()),
     path("status/<pk>/", StatusDetailView.as_view()),
     path("status/update/<pk>/", StatusUpdateView.as_view()),
